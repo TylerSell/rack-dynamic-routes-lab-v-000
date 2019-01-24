a042acad65092c3d2f429b2b76f5724158d4827d
tylerselldesign <tylerselldesign@gmail.com>
2019-01-24 03:28:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,24 @@
 class Application 
   
+  @@items = []
+  
   def call(env)
     resp = Rack::Response.new 
     req = Rack::Request.new 
     
-    if req.path == "/items/<ITEM NAME>"
+    if req.path.match == "/items/"
+      item_name = req.path.split("/items/").last 
+      if item = @@items.find{|i| i.name == item_name}
+        resp.write item.price 
+      else 
+        resp.write "Item not found"
+        resp.status = 400 
+      end
+    else 
+      resp.write "Path not found"
+      resp.status = 404
+    end
+    resp.finish 
   end
   
 end
